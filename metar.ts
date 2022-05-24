ede4d88419e6420248c283b70f84e65cc435432b
Kyle <kporter@kplink.net>
2022-05-23 21:50:44 -0700

    Update metar.ts
    Adds density altitude calculation and display to METAR request replies

--- metar.ts
+++ metar.ts
@@ -39,6 +39,10 @@ const sendMetarMessage = async (metar: string, msg: Message) => {
 		embed.addField('Temperature :thermometer:', `${m.temperature.celsius}°C (${m.temperature.farenheit || Math.round(m.temperature.celsius * 9 / 5)}°F)`);
 		embed.addField('Dewpoint :droplet:', `${m.dewpoint.celsius}°C (${m.dewpoint.farenheit || Math.round(m.dewpoint.celsius * 9 / 5)}°F)`);
 		embed.addField('Altimeter :chart_with_upwards_trend:', `${m.altimeter.inches} inHg (QNH ${m.altimeter.millibars})`);
+		var da = calcDA(m.station,m.temperature.celsius,m.altimeter.inches);
+		if(da != null) {
+			embed.addField('Density Altitude :high_brightness:', `${da} ft`);
+		}
 	}
 
 	await msg.channel.send(embed);
@@ -64,3 +68,15 @@ export default async (msg: Message) => {
 	}
 	await sendMetarMessage(metar, msg);
 };
+
+function calcDA(icao_id:string,temp:number,alt_set:number) { // Calculate density altitude for ZAB airfields
+    icao_id = icao_id.toUpperCase();
+    var elevation: {[key:string]:number} = {KAMA:3649,KCVS:4295,KROW:3671,KELP:3962,KBIF:3947,KHMN:4093,KABQ:5355,KAEG:5837,KSAF:6349,KSKX:7095,KFHU:4719,KTUS:2643,KDMA:2704,KRYN:2419,KPHX:1135,KIWA:1384,KFFZ:1394,KGYR:969,KLUF:1085,KGEU:1071,KDVT:1478,KSDL:1510,KGXF:883,KPRC:5045,KSEZ:4831,KFLG:7015,KINW:4941,KGUP:6472};
+    if(elevation.hasOwnProperty(icao_id)) {
+        var press_alt = ((29.92 - alt_set) * 1000) + elevation[icao_id];
+        var std_temp = 15 + ((elevation[icao_id] / 1000) * -2);
+        var density_alt = press_alt + (120 * (temp - std_temp));
+        return density_alt;
+    }
+    return null;
+}
